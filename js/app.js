2845d48e2302eb80fb5d3db001885b898477a678
intensity gamer <intensity67@gmail.com>
2019-02-20 08:21:29 +0800

    initiate paginate

--- js/app.js
+++ js/app.js
@@ -9,12 +9,36 @@ var app = new Vue({
 
       }, 
 
+        searchKey: '',
+        currentPage: 0,
+        itemsPerPage: 50,
+        resultCount: 0
+
 	 	mounted() {
 	          axios.get('get_passers').then(response => {
 	            this.exam_passers = response.data
 	          })
-	    }
-
-
-
-});
+	    },
+   	
+   	computed: {
+        totalPages: function() {
+          return Math.ceil(this.resultCount / this.itemsPerPage)
+        }
+    },
+    methods: {
+        setPage: function(pageNumber) {
+          this.currentPage = pageNumber
+        }
+    },
+    filters: {
+        paginate: function(list) {
+            this.resultCount = list.length
+            if (this.currentPage >= this.totalPages) {
+              this.currentPage = this.totalPages - 1
+            }
+            var index = this.currentPage * this.itemsPerPage
+            return list.slice(index, index + this.itemsPerPage)
+        }
+    }
+})
+ 
--- resources/views/list_vue.blade.php
+++ resources/views/list_vue.blade.php
@@ -4,53 +4,78 @@
    
 @section('content')
 
-<div id="list_passers">
+<style type="text/css">
 
+  a {
+    color: #999;
+  }
+  .current {
+    color: red;
+  }
+  ul {
+    padding: 0;
+    list-style-type: none;
+  }
+  li {
+    display: inline;
+    margin: 5px 5px;
+  }
+
+  a.first::after {
+    content:'...'
+  }
+
+  a.last::before {
+    content:'...'
+  }
+
+</style>
+
+<div id="list_passers">
+ 
     <table>
 
-      <thead>
-              <tr>
-                <td> Examinee ID
-                <td> Examinee
-                <td> Campus
-                <td> School
-                <td> Divisions
-            
-        </thead>
+        <thead>
+                <tr>
+                  <td> Examinee ID
+                  <td> Examinee
+                  <td> Campus
+                  <td> School
+                  <td> Divisions
+              
+          </thead>
 
-      <tbody>
-            
-    <paginate
-      :page-count="10"
-      :container-class="pagination"
-      :prev-text="prev"
-      :next-text="next"
-      :click-handler="clickCallback">
-     
-      <span slot="prevContent">Changed previous button</span>
-      <span slot="nextContent">Changed next button</span>
+        <tbody>
+    
+          <tr v-for="passer in exam_passers | paginate">
 
-        <tr v-for="passer in exam_passers">
+            <td>@{{ passer.id }}</td>
+            <td>@{{ passer.examinee }}</td>
+            <td>@{{ passer.campus_eligibility }}</td>
+            <td>@{{ passer.school }}</td>
+            <td>@{{ passer.division }}</td>
 
-          <td>@{{ passer.id }}</td>
-          <td>@{{ passer.examinee }}</td>
-          <td>@{{ passer.campus_eligibility }}</td>
-          <td>@{{ passer.school }}</td>
-          <td>@{{ passer.division }}</td>
+          </tr>
 
-        </tr>
+   
+        </tbody>
 
-      </tbody>
+      </table>
 
-    </table>
+      <ul>
+          <li v-for="pageNumber in totalPages" v-if="Math.abs(pageNumber - currentPage) < 3 || pageNumber == totalPages - 1 || pageNumber == 0">
+          <a href="#" @click="setPage(pageNumber)"  :class="{current: currentPage === pageNumber, last: (pageNumber == totalPages - 1 && Math.abs(pageNumber - currentPage) > 3), first:(pageNumber == 0 && Math.abs(pageNumber - currentPage) > 3)}"> @{{ pageNumber+1 }}</a>
+          </li>
+      </ul>
 
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 
+<script src="https://unpkg.com/vuejs-paginate@0.9.0"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.js"></script>
 
-<script src="https://unpkg.com/vuejs-paginate@0.9.0"></script>
 
 <script src="{{ URL::asset('js/app.js') }}" ></script>
 
